1d3d3df7818d7877ee8a5a75258f07a581aebc2a
Bunnarith Heang <heangbunnarith@gmail.com>
2022-02-09 13:57:45 +0700

    fix height & health bar rotation

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -9,8 +9,35 @@ public class HealthBar : MonoBehaviour
 	public Slider slider;
 	public Gradient gradient;
 	public Image fill;
+	public GameObject player;
 
-	public void SetMaxHealth(int health)
+    private void LateUpdate()
+    {
+        if (player != null)
+        {
+			RotateTowardPlayer();
+		}
+    }
+
+	private void RotateTowardPlayer()
+    {
+		// Determine which direction to rotate towards
+		Vector3 targetDirection = player.transform.position - transform.position;
+
+		// The step size is equal to speed times frame time.
+		float singleStep = 3.5f* Time.deltaTime;
+
+		// Rotate the forward vector towards the target direction by one step
+		Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+
+		// Draw a ray pointing at our target in
+		Debug.DrawRay(transform.position, newDirection, Color.red);
+
+		// Calculate a rotation a step closer to the target and applies rotation to this object
+		transform.rotation = Quaternion.LookRotation(newDirection);
+	}
+
+    public void SetMaxHealth(int health)
 	{
 		slider.maxValue = health;
 		slider.value = health;
