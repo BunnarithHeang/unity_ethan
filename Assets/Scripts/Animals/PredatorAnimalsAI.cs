ccf122cf0606bb70e2625a9ec16431818792568f
Venxing lay <venxinglay123@gmail.com>
2022-02-11 13:01:29 +0700

    fixing animal moving

--- Assets/Scripts/Animals/PredatorAnimalsAI.cs
+++ Assets/Scripts/Animals/PredatorAnimalsAI.cs
@@ -1,165 +1,152 @@
-//using UnityEngine;
-//using System.Collections;
-//using UnityEngine.AI;
-
-///// <summary>
-///// Creates wandering behaviour for a CharacterController.
-///// </summary>
-//[RequireComponent(typeof(CharacterController))]
-//public class PredatorAnimalsAI : MonoBehaviour
-//{
-//	public float speed = 0.5f;
-//	public float directionChangeInterval = 1;
-//	public float maxHeadingChange = 30;
-//	private Animator anim;
-//	private NavMeshAgent agent;
-
-//	CharacterController controller;
-//	float heading;
-//	Vector3 targetRotation;
-
-//	void Awake()
-//	{
-//		controller = GetComponent<CharacterController>();
-//		anim = GetComponent<Animator>();
-//		agent = GetComponent<NavMeshAgent>();
-
-//		// Set random initial rotation
-//		heading = Random.Range(0, 360);
-//		transform.eulerAngles = new Vector3(0, heading, 0);
-
-//		StartCoroutine(NewHeading());
-//	}
-
-//	void Update()
-//	{
-//		transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, targetRotation, Time.deltaTime * directionChangeInterval);
-//		var forward = transform.TransformDirection(Vector3.forward);
-//		Vector3 newPosition = RandomNavSphere(transform.position, 10, -1);
-
-//		controller.SimpleMove(newPosition * speed * Time.deltaTime);
-//        anim.SetFloat("Speed", 0.5f);
-//	}
-
-//	/// <summary>
-//	/// Repeatedly calculates a new direction to move towards.
-//	/// Use this instead of MonoBehaviour.InvokeRepeating so that the interval can be changed at runtime.
-//	/// </summary>
-//	IEnumerator NewHeading()
-//	{
-//		while (true)
-//		{
-//			NewHeadingRoutine();
-
-//			yield return new WaitForSeconds(directionChangeInterval);
-//		}
-//	}
-
-//	/// <summary>
-//	/// Calculates a new direction to move towards.
-//	/// </summary>
-//	void NewHeadingRoutine()
-//	{
-//		var floor = transform.eulerAngles.y - maxHeadingChange;
-//		var ceil = transform.eulerAngles.y + maxHeadingChange;
-//		heading = Random.Range(floor, ceil);
-//		targetRotation = new Vector3(0, heading, 0);
-
-//    }
-//	public Vector3 RandomNavSphere(Vector3 origin, float distance, int layerMask)
-//    {
-//        Vector3 randomDirection = Random.insideUnitSphere * distance;
-//        randomDirection += origin;
-
-
-//        NavMeshHit navHit;
-//        NavMesh.SamplePosition(randomDirection, out navHit, distance, layerMask);
-
-//        return navHit.position;
-//    }
-
-//}
-
-
-
-
-
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum EnemyState
+{
+    Pursue, Attack, Die, Search
+}
+
 public class PredatorAnimalsAI : MonoBehaviour
 {
 
-
-    private float radius = 1;
+    private float radius = 20;
+    private float lookRadius = 3.0f;
+    private int MaxHealth = 100;
+    private int currentHealth;
 
     private NavMeshAgent agent;
     private Animator anim;
-    private IEnumerator coroutine;
-    private bool isWalking = false;
-    private bool isWandering = false;
+    //create HealthBarz object
+    public HealthBar healthbar;
+
+
+
+    private EnemyState currentState = EnemyState.Search;
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
-
+        currentHealth = MaxHealth;
+        healthbar.SetMaxHealth(MaxHealth);
     }
-
 
     private void Update()
     {
-        if (!isWandering)
+
+        float distance = Vector3.Distance(GameObject.FindGameObjectWithTag("Player").transform.position, transform.position);
+
+        bool PlayerInAttackRange = distance <= lookRadius;
+        
+        switch (currentState)
         {
-            StartCoroutine(Wander());
-        }
-        if (isWalking)
-        {
-            Walking();
-            anim.SetFloat("Speed", 0.5f);
+            case EnemyState.Search:
+                Searching();
+                if (PlayerInAttackRange)
+                {
+                    Pursue();
+                    ChangeState(EnemyState.Attack);
+                }
+                break;
+            case EnemyState.Pursue:
+                if (currentHealth <= 0)
+                {
+                    ChangeState(EnemyState.Die);
+                }
+                // Players enter the attack state within the attack range.
+                if (PlayerInAttackRange)
+                {
+                    Pursue();
+                    ChangeState(EnemyState.Attack);
+                }
+                break;
+
+            case EnemyState.Attack:
+
+                if (currentHealth <= 0)
+                {
+                    ChangeState(EnemyState.Die);
+                }
+                if (!PlayerInAttackRange)
+                {
+                    ChangeState(EnemyState.Search);
+                }
+                break;
+            
+            case EnemyState.Die:
+                Debug.Log("zombie death");
+                break;
         }
     }
 
+    public void ChangeState(EnemyState state)
+    {
+        currentState = state;
+    }
 
-    //private IEnumerator StartAnimation(float waitingTime)
+
+    //private void Start()
     //{
-    //    while (true)
+    //    agent = GetComponent<NavMeshAgent>();
+    //    anim = GetComponent<Animator>();
+    //    currentHealth = MaxHealth;
+    //    healthbar.SetMaxHealth(MaxHealth);
+    //}
+
+
+    //private void Update()
+    //{
+    //    float distance = Vector3.Distance(GameObject.FindGameObjectWithTag("Player").transform.position, transform.position);
+
+    //    bool isNearPlayer = distance <= lookRadius;
+
+
+    //    timer += Time.deltaTime;
+
+    //    if (isNearPlayer)
     //    {
-    //        //anim.SetFloat("Speed", 0.0f);
-    //        yield return new WaitForSeconds(waitingTime);
-    //        AnimatePosition();
-    //        yield return new WaitForSeconds(waitingTime);
+
+    //        // Move towards the player
+    //        Pursue();
+    //        //cancle timer to 0
+    //        timer = 0.0f;
+    //    }
+    //    else if (timer > 13.0f)
+    //    {
+    //        timer = 0;
+    //        agent.SetDestination(transform.position);
 
     //    }
 
+    //    else if (timer > 10.0f)
+    //    {
+    //        Pursue();
+
+    //    }
+    //    else
+    //    {
+
+    //        agent.SetDestination(transform.position);
+    //    }
     //}
-    private void Walking()
+
+    private void Searching()
     {
+
         Vector3 newPosition = RandomNavSphere(transform.position, radius, -1);
         agent.SetDestination(newPosition);
+
+        anim.SetFloat("Speed", 0.5f);
     }
 
-    private IEnumerator Wander()
+    private void Pursue()
     {
-        int walkWait = Random.Range(5, 10);
-        int walkTime = Random.Range(5, 10);
-        //int rotateTime = Random.Range(1, 3);
-        //int rotateWait = Random.Range(1, 5);
+        agent.SetDestination(GameObject.FindGameObjectWithTag("Player").transform.position);
 
-        isWandering = true;
-
-        yield return new WaitForSeconds(walkWait);
-        isWalking = true;
-        yield return new WaitForSeconds(walkTime);
-        isWalking = false;
-        anim.SetFloat("Speed", 0.0f);
-        isWandering = false;
+        anim.SetFloat("Speed", 1.0f);
     }
-
-
 
 
     public Vector3 RandomNavSphere(Vector3 origin, float distance, int layerMask)
@@ -174,6 +161,22 @@ public class PredatorAnimalsAI : MonoBehaviour
         return navHit.position;
     }
 
+    void TakeDamage(int demage)
+    {
+        currentHealth -= demage;
+        healthbar.SetHealth(currentHealth);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TakeDamage(5);
+
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 
 
 }
