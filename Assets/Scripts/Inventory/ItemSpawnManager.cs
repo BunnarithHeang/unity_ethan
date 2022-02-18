4a2903bf5e4f40f3c56972d6d9f85cbf8e3caae8
Bunnarith Heang <heangbunnarith@gmail.com>
2022-02-18 15:36:51 +0700

    bridge

--- Assets/Scripts/Inventory/ItemSpawnManager.cs
+++ Assets/Scripts/Inventory/ItemSpawnManager.cs
@@ -6,22 +6,35 @@ public class ItemSpawnManager : MonoBehaviour
 {
     [SerializeField] private List<GameObject> weaponPrefabs;
     [SerializeField] private List<GameObject> healthPrefabs;
-    [SerializeField] private Transform[] SpawnPoints;
+    private List<Transform> spawnPoints = new List<Transform>();
 
     public List<InventoryItem> inventoryItemObjects { get; private set; }
 
-    private float spawnTime = 5.0f;
+    private float spawnTime = 100.0f;
     private float radius = 1.0f;
 
     private void Awake()
     {
         inventoryItemObjects = new List<InventoryItem>();
+
+        
+        List<GameObject> cp = FindGameObjectInChildWithTag(gameObject, "ItemCheckPoint");
+        for (int i = 0; i < cp.Count; ++i)
+        {
+            spawnPoints.Add(cp[i].transform);
+        }
     }
 
     private void Start()
     {
-        InvokeRepeating("SpawnAnimals", spawnTime, spawnTime);
+        InvokeRepeating("SpawnRandomItems", spawnTime, 30.0f);
+       
         SpawnWeapons();
+
+        for (int i = 0; i < 5; ++i)
+        {
+            SpawnRandomItems();
+        }
     }
 
     private InventoryItem.ItemType GetTypeFromTag(string tag)
@@ -39,26 +52,38 @@ public class ItemSpawnManager : MonoBehaviour
 
     void SpawnWeapons()
     {
-        Vector3 spawnPos0 = SpawnPoints[0].position + (Vector3)Random.insideUnitCircle * radius;
-        Instantiate(healthPrefabs[0], spawnPos0, SpawnPoints[0].rotation);
+        Vector3 spawnPos0 = spawnPoints[0].position + (Vector3)Random.insideUnitCircle * radius;
+        Instantiate(weaponPrefabs[0], spawnPos0, spawnPoints[0].rotation);
 
-        Vector3 spawnPos1 = SpawnPoints[1].position + (Vector3)Random.insideUnitCircle * radius;
-        Instantiate(healthPrefabs[0], spawnPos1, SpawnPoints[1].rotation);
+        Vector3 spawnPos1 = spawnPoints[1].position + (Vector3)Random.insideUnitCircle * radius;
+        Instantiate(weaponPrefabs[0], spawnPos1, spawnPoints[1].rotation);
     }
 
-    void SpawnAnimals()
+    private void SpawnRandomItems()
     {
-        int index = Random.Range(0, healthPrefabs.Count);
 
-        if (index == 0)
+        int posIndex = Random.Range(0, spawnPoints.Count);
+        int itemIndex = Random.Range(0, healthPrefabs.Count);
+
+        Debug.Log(spawnPoints[posIndex].position);
+
+        Vector3 spawnPos = spawnPoints[posIndex].position + (Vector3)Random.insideUnitCircle * radius;
+        Instantiate(healthPrefabs[itemIndex], spawnPos, spawnPoints[0].rotation);
+    }
+
+    public static List<GameObject> FindGameObjectInChildWithTag(GameObject parent, string tag)
+    {
+        Transform t = parent.transform;
+        List<GameObject> objects = new List<GameObject>();
+
+        for (int i = 0; i < t.childCount; i++)
         {
-            Vector3 spawnPos = SpawnPoints[0].position + (Vector3)Random.insideUnitCircle * radius;
-            Instantiate(healthPrefabs[index], spawnPos, SpawnPoints[0].rotation);
+            if (t.GetChild(i).gameObject.tag == tag)
+            {
+                objects.Add(t.GetChild(i).gameObject);
+            }
         }
-        else if (index == 1)
-        {
-            Vector3 spawnPos = SpawnPoints[1].position + (Vector3)Random.insideUnitCircle * radius;
-            Instantiate(healthPrefabs[index], spawnPos, SpawnPoints[1].rotation);
-        }
+
+        return objects;
     }
 }
